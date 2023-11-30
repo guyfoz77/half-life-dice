0d3c7002b0fc23c4d6fc96d495f5f7c02676df98
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-11-30 18:12:24 +0000

    Write basic dice roll script

---
+++ src/scripts/scripts.js
@@ -0,0 +1,20 @@
+import { rollData } from '../data/data'
+
+export function rollDice(sides) {
+  if (sides <= 1) {
+    console.error('A die must have at least 2 sides')
+    return null
+  }
+  const result = Math.floor(Math.random() * sides) + 1
+  return result
+}
+
+export function halfLifeStep(startingDice, sides) {
+  let decayedDice = 0
+  for (let i = 0; i < startingDice; i++) {
+    const diceResult = rollDice(sides)
+    if (diceResult == 1) decayedDice++
+  }
+  console.log(decayedDice)
+  // return decayedDice
+}
